09bdccfc5f2a5fcbdcc54dddadc4c17515acee86
PNSVN3035 <premsaivenkat7435@gmail.com>
2023-12-20 21:56:33 -0500

    super-calling-statement-usage-in-constructor

---
+++ ExcelREvening/Backend/CoreJava/06-inheritence/01-is-a/src/N.java
@@ -0,0 +1,24 @@
+class M
+{
+	M()
+	{
+		System.out.println("M()");
+	}
+}
+
+class N extends M
+{
+	N()
+	{
+		System.out.println("N()");
+	}
+	public static void main(String[] args) 
+	{
+		N n1 = new N();
+		System.out.println("from N main");
+	}
+}
+
+/*
+- There is class called Object that is the in-built class.
+*/
