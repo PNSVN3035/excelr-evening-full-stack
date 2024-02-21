cb03e0294e060a235d366cc7c32ce06bdc4f3ae1
PNSVN3035 <premsaivenkat7435@gmail.com>
2024-02-21 01:32:48 -0500

    Annotations, JDK8 Features

--- ExcelREvening/Backend/CoreJava/25-generics/src/M12.java
+++ ExcelREvening/Backend/CoreJava/25-generics/src/M12.java
@@ -7,11 +7,11 @@ class M12
 	public static void main(String [] args)
 	{
 		
-		Z <P> t1 = new Z <P> ();
-		Z <Q> t2 = new Z <Q> ();
-		Z <R> t3 = new Z <R> ();
-		Z <S> t4 = new Z <S> ();
-		Z <T> t5 = new Z <T> ();
+		Z <P> t1 = new Z<P>();
+		Z <Q> t2 = new Z<Q>();
+		Z <R> t3 = new Z<R>();
+		Z <S> t4 = new Z<S>();
+		Z <T> t5 = new Z<T>();
 		Z <String> t6 = new Z <String> ();
 		System.out.println("done");
 	}
